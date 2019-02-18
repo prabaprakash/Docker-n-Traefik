e6f6078645c3c8ec8505fbaaeb9054f24ce9c351
prabaprakash <prabakaran1993@gmail.com>
2019-02-18 11:34:23 -0700

    Devops - Docker & Traefik

---
+++ steps.sh
@@ -0,0 +1,17 @@
+sudo apt-get install apache2-utils
+htpasswd -nb admin password
+admin:$apr1$ruca84Hq$mbjdMZBAG.KWn7vfN/SNK/
+nano traefik.toml
+
+docker run -d \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $PWD/traefik.toml:/traefik.toml \
+  -v $PWD/acme.json:/acme.json \
+  -p 80:80 \
+  -p 443:443 \
+  -l traefik.frontend.rule=Host:monitor.vaayadi.in \
+  -l traefik.port=8080 \
+  --network proxy \
+  --name traefik \
+  traefik:1.3.6-alpine --docker
+
