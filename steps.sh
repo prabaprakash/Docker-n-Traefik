23af4cab72c214c1804144120f5373eae5e613eb
Ubuntu <azure_key@vaayadivm.0h34ju321whe1k2lonw4klgrqg.ix.internal.cloudapp.net>
2020-08-26 16:08:59 +0000

    #Traefik logs configuration

--- steps.sh
+++ steps.sh
@@ -25,6 +25,7 @@ docker run -d \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v $PWD/traefik.toml:/traefik.toml \
   -v $PWD/acme.json:/acme.json \
+  -v $PWD/logs:/logs \
   -p 80:80 \
   -p 443:443 \
   -l traefik.frontend.rule=Host:monitor.southindiantrekkers.org \
